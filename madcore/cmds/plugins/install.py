c1df170013bf8a0e5e2361795082743e9184f3df
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-02-05 14:09:22 +0200

    Allow to input all params from bash script, #69

--- madcore/cmds/plugins/install.py
+++ madcore/cmds/plugins/install.py
@@ -12,12 +12,27 @@ logger = logging.getLogger(__name__)
 class PluginInstall(PluginManagement, PluginCommand):
     _description = "Install madcore plugins"
     plugin_job = 'deploy'
+    _plugin_name = None
+
+    @property
+    def plugin_name(self):
+        if not self._plugin_name:
+            self._plugin_name = self.get_plugin_name_from_input_args()
+        return self._plugin_name
+
+    def get_parser(self, prog_name):
+        parser = super(PluginInstall, self).get_parser(prog_name)
+
+        if self.plugin_name in self.get_plugin_names():
+            params = self.get_plugin_job_all_params(self.plugin_name, self.plugin_job)
+            parser = self.add_params_to_arg_parser(parser, params)
+
+        return parser
 
     def take_action(self, parsed_args):
-        # TODO@geo investigate and see if we can make the plugin commands available in the interactive mode
-        # after plugin was installed. Currently we need to exit the cli interactive mode to see the new commands
         plugin_name = parsed_args.plugin_name
 
+        # after plugin was installed. Currently we need to exit the cli interactive mode to see the new commands
         if not parsed_args.force and config.is_plugin_installed(plugin_name):
             self.logger.info("[%s] Plugin already installed.", plugin_name)
             return 0
