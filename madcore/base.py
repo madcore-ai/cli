e9fd2243b2ad9602bd315aa28d9311d358b8210d
Chirica Gheorghe <chiricagheorghe@gmail.com>
2017-04-04 05:52:16 +0300

    Fixed hardcoded domin index branch

--- madcore/base.py
+++ madcore/base.py
@@ -73,7 +73,7 @@ class MadcoreBase(object):
                 index_content = content_file.read()
                 domains = json.loads(index_content)
         else:
-            domains_url = 'https://raw.githubusercontent.com/madcore-ai/plugins/development/domain-index.json'
+            domains_url = 'https://raw.githubusercontent.com/madcore-ai/plugins/%s/domain-index.json' % self.env_branch
             domains = requests.get(domains_url).json()
 
         return domains
