9e205e9c135b6e557849170101b7f7c66950e2e2
Peter Styk <peter@styk.tv>
2018-04-28 07:12:53 +0100

    updates to setup versioning

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except IOError:
     long_description = ''
 
 setup(
-    use_scm_version=True,
+    use_scm_version = {"root": "..", "relative_to": __file__},
     setup_requires=['setuptools_scm'],
 
     name=PROJECT,
