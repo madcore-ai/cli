fe8ba2f5a1ca5e32a1dc03b2dbfc75e672c5d346
Peter Styk <peter@styk.tv>
2018-04-29 11:12:45 +0100

    setup dist vs git version

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools.dist import Distribution
 #from madcore.cmd import Cmd
 import subprocess
 import sys
+import pkg_resources
 
 
 #class BinaryDistribution(Distribution):
@@ -16,28 +17,30 @@ import sys
 
 def get_semantic_version():
     global VERSION
+    try:
+        return pkg_resources.get_distribution("madcore").version
+    except:
+        proc1 = subprocess.Popen("git describe --tags", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        out = proc1.communicate()
 
-    proc1 = subprocess.Popen("git describe --tags", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    out = proc1.communicate()
+        if proc1.returncode != 0:
+            sys.stdout.write("madcore must install from cloned folder. make sure .git folder exists\n")
+            sys.stdout.write(out[1])
+            raise SystemExit(32)
 
-    if proc1.returncode != 0:
-        sys.stdout.write("madcore must install from cloned folder. make sure .git folder exists\n")
-        sys.stdout.write(out[1])
-        raise SystemExit(32)
+        v = out[0].replace('\n','')
 
-    v = out[0].replace('\n','')
-
-    if v.startswith('v.'):
-        v = v[2:]
-    elif v.startswith('v'):
-        v = v[1:]
-    li = v.split('.')
-    lii = li[1].split('-')
-    if len(lii) == 3:
-        v = '{0}.{1}.{2}'.format(li[0],lii[0],lii[1])
-    else:
-        v = '{0}.{1}'.format(li[0], li[1])
-    return v
+        if v.startswith('v.'):
+            v = v[2:]
+        elif v.startswith('v'):
+            v = v[1:]
+        li = v.split('.')
+        lii = li[1].split('-')
+        if len(lii) == 3:
+            v = '{0}.{1}.{2}'.format(li[0],lii[0],lii[1])
+        else:
+            v = '{0}.{1}'.format(li[0], li[1])
+        return v
 
 
 VERSION = get_semantic_version()
